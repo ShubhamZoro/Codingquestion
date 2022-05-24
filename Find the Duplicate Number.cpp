500fe355d010ab32a2c9cd591a83dc0e2a4d15d3
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2022-05-24 21:57:50 +0530

    Just adding the question description

--- Find the Duplicate Number.cpp
+++ Find the Duplicate Number.cpp
@@ -1,3 +1,8 @@
+//Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+//There is only one repeated number in nums, return this repeated number.
+//You must solve the problem without modifying the array nums and uses only constant extra space.
+//Input: nums = [3,1,3,4,2]
+//Output: 3
 #include<iostream>
 using namespace std;
 #include<vector>
