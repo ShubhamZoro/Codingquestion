432e682ca2f70c0758ccfd9726dc82bf5d715139
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2023-05-31 20:07:40 +0530

    Update search in sorted matrix.cpp

--- search in sorted matrix.cpp
+++ search in sorted matrix.cpp
@@ -12,3 +12,25 @@ vector<int> searchInSortedMatrix(vector<vector<int>> matrix, int target) {
   }
   return {-1,-1};
 }
+
+#O(row+col)
+#include <vector>
+using namespace std;
+
+vector<int> searchInSortedMatrix(vector<vector<int>> matrix, int target) {
+ int col=matrix[0].size()-1;
+  
+ int row=0;
+  while(row<matrix.size() && col>=0){
+    if(matrix[row][col]>target){
+      col--;
+    }
+    else if(matrix[row][col]<target){
+      row++;
+    }
+    else{
+      return {row,col};
+    }
+  }
+  return{-1,-1};
+}
