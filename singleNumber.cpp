123ed164874014cb4cd744fa5a44235f48e996d9
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2023-09-05 20:22:51 +0530

    Create singleNumber.cpp

---
+++ singleNumber.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        unordered_map<int,int>umap;
+        for(int i=0;i<nums.size();i++){
+            umap[nums[i]]+=1;
+        }
+        
+        for(auto x:umap){
+            if (x.second==1){
+                return x.first;
+            }
+        }
+        return -1;
+    }
+};
