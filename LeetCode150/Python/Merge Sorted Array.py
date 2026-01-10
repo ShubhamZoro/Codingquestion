4ef31dfef0b6f846c50118a4b9b6ce6f6170d7e7
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-01-10 21:06:43 +0530

    Add files via upload

---
+++ LeetCode150/Python/Merge Sorted Array.py
@@ -0,0 +1,29 @@
+class Solution:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        res = []
+        i = 0
+        j = 0
+
+        
+        while i < m and j < n:
+            if nums1[i] > nums2[j]:
+                res.append(nums2[j])
+                j += 1
+            else:
+                res.append(nums1[i])
+                i += 1
+
+        
+        if i < m:
+            res.extend(nums1[i:m])
+        if j < n:
+            res.extend(nums2[j:n])
+
+        
+        nums1[:] = res
+
+
+            
