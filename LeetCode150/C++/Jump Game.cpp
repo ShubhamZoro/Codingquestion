4eb325ac01da9be86fc1fcf14e4a680107c5973f
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-01-15 15:29:46 +0530

    Update Jump Game.cpp

--- LeetCode150/C++/Jump Game.cpp
+++ LeetCode150/C++/Jump Game.cpp
@@ -35,3 +35,27 @@ int t[10001];
     }
 
 };
+-----------------------------------------------------
+    class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+         int n = nums.size();
+        
+        vector<int> t(n, false);
+        
+        
+        t[0] = true;
+        
+        for(int i = 1; i<n; i++) {
+            for(int j = i-1; j>=0; j--) {
+                if(j + nums[j] >= i && t[j]) {
+                    t[i] = true;
+                    break;
+                }
+            }
+        }
+        
+        
+        return t[n-1];
+    }
+};
