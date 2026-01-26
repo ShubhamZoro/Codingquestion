c3d70c9efb7b8fa0667967cb563f937956dec2f3
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-01-26 12:07:43 +0530

    Refactor insert and remove methods for clarity

--- Insert Delete GetRandom O(1).cpp
+++ Insert Delete GetRandom O(1).cpp
@@ -1,48 +1,34 @@
-#include <cstdlib>  
 class RandomizedSet {
 public:
-    unordered_map<int,int> m;
-vector<int>v;    
+unordered_map<int,int> m;
+vector<int>v;  
     RandomizedSet() {
         
     }
     
     bool insert(int val) {
-        // if(umap.find(val)==umap.end()){
-        //     umap[val]=list.size();
-        //     list.push_back(val);
-        //     return true;
-        // }
-        // return false;
-        if(m.count(val))
-       return false;
-       m[val]=v.size();
-       v.push_back(val);
-       return true;
+        if(m.find(val)!=m.end()){
+            return false;
+        }
+        m[val]=v.size();
+        v.push_back(val);
+        return true;
     }
     
     bool remove(int val) {
-        // if(umap.find(val)!=umap.end()){
-        //     int index=umap[val];
-        //     int value=list.back();
-        //     list[index]=value;
-        //    list.pop_back();
-        //     umap.erase(val);
-        //     return true;
-        // }
-        // return false;
-        if(!m.count(val))
-      return false;
-      int p=v.back();
-      m[p]=m[val];
-      v[m[val]]=p;
-      v.pop_back();
-      m.erase(val);
-      return true;
+        if(m.find(val)==m.end()){
+            return false;
+        }
+        int p=v.back();
+        m[p]=m[val];
+        v[m[val]]=p;
+        v.pop_back();
+        m.erase(val);
+        return true;
     }
     
     int getRandom() {
-        return v[rand()% v.size()];
+        return v[rand()%v.size()];
     }
 };
 
