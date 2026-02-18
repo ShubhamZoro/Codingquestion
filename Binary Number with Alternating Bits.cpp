733bc91a70d90744fff4d6660371f5bff7a8e899
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-02-18 12:53:43 +0530

    Update Binary Number with Alternating Bits.cpp

--- Binary Number with Alternating Bits.cpp
+++ Binary Number with Alternating Bits.cpp
@@ -18,3 +18,14 @@ public:
 };
 
 ---------------
+class Solution {
+public:
+    bool hasAlternatingBits(int n) {
+         bitset<32> b(n);
+        int bits = log2(n);
+        for(int i = 0; i<=bits; i++) {
+            if(b[i] == b[i+1]) return false;
+        }
+        return true;
+    }
+};
