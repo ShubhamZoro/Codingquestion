1acd6edc014fc73db6d2f205bfde3ff17e56f8da
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-03-05 09:32:50 +0530

    Add solution for minimum changes to alternating binary string
    Implement a function to calculate minimum changes required to convert a binary string to an alternating pattern.

---
+++ Minimum Changes To Make Alternating Binary String.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int minOperations(string s) {
+        string s1="",s2="";
+
+        int n=s.size();
+        for(int i=0;i<n;i++){
+            if(i%2==0){
+                s1+="1";
+            }
+            else{
+                s1+="0";
+            }
+
+             if(i%2!=0){
+                s2+="1";
+            }
+            else{
+                s2+="0";
+            }
+        }
+        int count1=0;
+        int count2=0;
+        for(int i=0;i<n;i++){
+            if(s[i]!=s1[i]){
+                count1++;
+            }
+            if(s[i]!=s2[i]){
+                count2++;
+            }
+        }
+        return min(count1,count2);
+    }
+};
