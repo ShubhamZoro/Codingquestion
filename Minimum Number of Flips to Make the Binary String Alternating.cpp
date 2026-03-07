9cf8a2f6459ee9cad60ed1aff2049f44eb3ab189
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-03-07 09:10:28 +0530

    Optimize minFlips function for better space complexity
    Optimized the solution for minimum flips with better space complexity by using two alternating strings and reducing the count of flips in a single pass.

--- Minimum Number of Flips to Make the Binary String Alternating.cpp
+++ Minimum Number of Flips to Make the Binary String Alternating.cpp
@@ -53,3 +53,62 @@ public:
 
     }
 };
+
+---------------------------------------------------
+// optimzed solution with better space complexity
+    class Solution {
+public:
+    int minFlips(string s) {
+        
+        string s1="";
+        string s2="";
+        
+        int m=s.size();
+        for(int i=0;i<2*m;i++){
+            if(i%2!=0){
+                s1+="0";
+                s2+="1";
+            }
+            else{
+                s1+="1";
+                s2+="0";
+            }
+        }
+
+        int count_s1=0;
+        int count_s2=0;
+
+        for(int i=0;i<m;i++){
+                if(s[i]!=s1[i]){
+                    count_s1++;
+                }
+
+                if(s[i]!=s2[i]){
+                    count_s2++;
+                }
+        }
+        int res=min(count_s1,count_s2);
+       
+        for(int i=1;i<m;i++){
+            if(s[i-1]!=s1[i-1]){
+                count_s1--;
+            }
+            int j=i+m-1;
+            if(s[j%m]!=s1[i+m-1]){
+                count_s1++;
+            }
+
+            if(s[i-1]!=s2[i-1]){
+                count_s2--;
+            }
+            if(s[j%m]!=s2[i+m-1]){
+                count_s2++;
+            }
+
+            res=res = min(res, min(count_s1, count_s2));;
+        }
+
+        return res;
+
+    }
+};
