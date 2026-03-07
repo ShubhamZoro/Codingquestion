a0667471d254e903f6c65811240199b12c99c36f
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-03-07 09:49:30 +0530

    Add intersection detection for two linked lists
    Implemented a solution to find the intersection node of two singly linked lists using constant space.

--- Intersection of Two Linked Lists.cpp
+++ Intersection of Two Linked Lists.cpp
@@ -26,3 +26,27 @@ public:
         return NULL;
     }
 };
+---------------------------------------
+// constant space
+    /**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+         ListNode* A = headA;
+        ListNode* B = headB;
+
+        while (A != B) {
+            A = (A != nullptr) ? A->next : headB;
+            B = (B != nullptr) ? B->next : headA;
+        }
+
+        return A; 
+    }
+};
