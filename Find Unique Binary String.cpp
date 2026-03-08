a1e506a38c6dd52a70c09dae0b5955faaf4b1a8b
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-03-08 08:46:10 +0530

    Update Find Unique Binary String.cpp

--- Find Unique Binary String.cpp
+++ Find Unique Binary String.cpp
@@ -30,3 +30,18 @@ public:
         return res;
     }
 };
+//-----------------------------------
+//Reducing complexity to O(n)
+
+class Solution {
+public:
+    string findDifferentBinaryString(vector<string>& nums) {
+        string result="";
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+            char ch=nums[i][i];
+            result+=(ch=='0') ?"1":"0";
+        }
+        return result;
+    }
+};
