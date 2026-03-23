3b6bdc7e0a6fbaf5129b39ffb6a4472e372e53bf
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-03-23 09:17:58 +0530

    Refactor binary search for lower and upper bounds

--- Find First and Last Position of Element in Sorted Array.cpp
+++ Find First and Last Position of Element in Sorted Array.cpp
@@ -1,30 +1,51 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
+    int lower_bound_binary_search(vector<int>& nums, int target) {
         int left = 0, right = nums.size() - 1;
-        int first = -1, last = -1;
-        
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] == target) {
-                first = mid;
-                last = mid;
-                while (first > 0 && nums[first - 1] == target) {
-                    first--;
-                }
-                while (last < nums.size() - 1 && nums[last + 1] == target) {
-                    last++;
-                }
-                break;
-            }
+
+        while (left < right) {
+            int mid = (left + right) / 2;
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } 
             else if (nums[mid] < target) {
                 left = mid + 1;
-            }
+            } 
             else {
-                right = mid - 1;
+                right = mid;  // move left
             }
         }
-        
-        return {first, last};
+
+        return (!nums.empty() && nums[left] == target) ? left : -1;
+    }
+
+    int upper_bound_binary_search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+
+        while (left < right) {
+            int mid = (left + right) / 2 + 1; // right bias
+
+            if (nums[mid] > target) {
+                right = mid - 1;
+            } 
+            else if (nums[mid] < target) {
+                left = mid + 1;
+            } 
+            else {
+                left = mid;  // move right
+            }
+        }
+
+        return (!nums.empty() && nums[right] == target) ? right : -1;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int lower = lower_bound_binary_search(nums, target);
+        int upper = upper_bound_binary_search(nums, target);
+        return {lower, upper};
     }
 };
