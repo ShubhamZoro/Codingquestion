ef6054e49044849369a667ed7964ae749550797c
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-03-24 09:45:19 +0530

    Implement function to group anagrams from a list

---
+++ Group Anagrams.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        vector<vector<string>>res;
+        unordered_map<string,vector<string>>umap;
+        for(auto i:strs){
+            string s=i;
+            sort(s.begin(),s.end());
+            umap[s].push_back(i);
+            
+        }
+        for(auto i:umap){
+            res.push_back(i.second);
+        }
+        return res;
+    }
+};
