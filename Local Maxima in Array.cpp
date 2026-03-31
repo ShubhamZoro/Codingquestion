844338ee421140d1b828f695dc18bec3e8515c71
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-03-31 10:50:36 +0530

    Optimal Solution local maxima function

--- Local Maxima in Array.cpp
+++ Local Maxima in Array.cpp
@@ -1,13 +1,39 @@
-from typing import List
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums) {
+        int n=nums.size();
+        if(n<=1){
+            return 0;
+        }
 
-def local_maxima_in_array(nums: List[int]) -> int:
-    # Write your code here
-    if(len(nums)<3):
-        return 0
-    for i in range(1,len(nums)):
-        if i==len(nums)-1 and nums[i]>nums[i-1]:
-            return i;
-        
-        if(nums[i]>nums[i-1] and nums[i]>nums[i+1]):
-            return i
-    return 0
+        for(int i=1;i<n;i++){
+            if(nums[i]>nums[i-1] && i==n-1){
+                return i;
+            }
+            if(nums[i]>nums[i-1] && nums[i]>nums[i+1]){
+                return i;
+            }
+        }
+        return 0;
+    }
+};
+
+/////////////////////////////////////
+
+class Solution {
+public:
+    int findPeakElement(vector<int>& nums){
+        int l=0;
+        int r=nums.size()-1;
+        while(l<r){
+            int mid=(l+r)/2;
+            if(nums[mid]<nums[mid+1]){
+                l=mid+1;
+            }
+            else{
+                r=mid;
+            }
+        }
+        return l;
+    }
+};
