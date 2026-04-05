3d843edfbe524e26c59f095b8e86010df3917978
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-04-05 11:09:53 +0530

    Refactor calculator to use long long for calculations
    Updated the calculator to use long long for larger number handling and improved readability.

--- Basic Calculator.cpp
+++ Basic Calculator.cpp
@@ -1,43 +1,45 @@
 class Solution {
 public:
     int calculate(string s) {
-        stack<int>st;
-        int result=0;
-        int number=0;
-        int sign=1;
-        for(auto i:s){
-            if(isdigit(i)){
-                number=number*10+(i-'0');
+        stack<long long> st;
+        long long number = 0;
+        long long result = 0;
+        long long sign = 1;
+
+        for (auto i : s) {
+            if (isdigit(i)) {
+                number = number * 10 + (i - '0');
             }
-            else if(i=='+'){
-                result+=number*sign;
-                number=0;
-                sign=1;
+            else if (i == '+') {
+                result += number * sign;
+                number = 0;
+                sign = 1;
             }
-            else if(i=='-'){
-                result+=number*sign;
-                number=0;
-                sign=-1;
+            else if (i == '-') {
+                result += number * sign;
+                number = 0;
+                sign = -1;
             }
-            else if(i=='('){
+            else if (i == '(') {
                 st.push(result);
                 st.push(sign);
-                result=0;
-                number=0;
-                sign=1;
+                result = 0;
+                number = 0;
+                sign = 1;
             }
-            else if(i==')'){
-                result+=number*sign;
-                number=0;
-                int stack_sign=st.top();
-                st.pop();
-                int stack_res=st.top();
-                st.pop();
-                result*=stack_sign;
-                result+=stack_res;
+            else if (i == ')') {
+                result += number * sign;
+                number = 0;
+
+                long long stack_sign = st.top(); st.pop();
+                long long stack_res = st.top(); st.pop();
+
+                result *= stack_sign;
+                result += stack_res;
             }
         }
-        result+=number*sign;
-        return result;
+
+        result += number * sign;
+        return (int)result;  // cast back if required
     }
 };
