f9df127351d293e4a40bfeb73447c2210e5ed804
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-04-06 10:54:55 +0530

    Add solution to remove adjacent duplicates in string
    Implement a solution to remove adjacent duplicates from a string using a stack.

---
+++ Remove All Adjacent Duplicates In String.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char>st;
+        string res="";
+        for(auto c : s){
+            if(!st.empty() && st.top()==c){
+                st.pop();
+            }
+            else{
+                st.push(c);
+            }
+        }
+        while(!st.empty()){
+            res=st.top()+res;
+            st.pop();
+        }
+        return res;
+
+    }
+};
