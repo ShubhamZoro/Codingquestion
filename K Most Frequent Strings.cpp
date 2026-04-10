6102889e2cba1d9759d1d429023efb32fec72b52
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-04-10 11:30:45 +0530

    Add topKFrequent method to find frequent strings
    Implemented a class Solution with a method to find the top K frequent strings from a list of words.

--- K Most Frequent Strings.cpp
+++ K Most Frequent Strings.cpp
@@ -19,3 +19,39 @@ def k_most_frequent_strings(strs: List[str], k: int) -> List[str]:
             result.append(sorted_items[i][0])
 
     return result
+///
+
+
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        vector<pair<int, string>> vp;
+        unordered_map<string, int> mp;
+        
+        for(string &word : words) {
+            mp[word]++;
+        }
+        
+        for(auto &it : mp) {
+            vp.push_back({it.second, it.first});
+        }
+        
+        auto lambda = [](pair<int, string>& p1, pair<int, string>& p2) {
+            if(p1.first == p2.first)
+                return p1.second < p2.second;
+            
+            return p1.first > p2.first;
+        };
+        
+        sort(begin(vp), end(vp), lambda);
+        
+        int i = 0;
+        vector<string> result(k);
+        while(i < k) {
+            result[i] = vp[i].second;
+            i++;
+        }
+        
+        return result;
+    }
+};
