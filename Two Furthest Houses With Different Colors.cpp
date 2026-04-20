28fd566cf0bb003492de83ae1feb200ffd1e666b
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-04-20 07:51:20 +0530

    Add maxDistance function for furthest houses
    Implement maxDistance function to calculate the maximum distance between two houses of different colors.

--- Two Furthest Houses With Different Colors.cpp
+++ Two Furthest Houses With Different Colors.cpp
@@ -14,3 +14,26 @@ public:
         return max1;
     }
 };
+
+// O(n)
+
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+         int n = colors.size();
+
+        int result = 0;
+
+        int i = 0;
+        while(colors[i] == colors[n-1]) i++;
+
+        result = max(result, abs(i - (n-1)));
+
+        i = n-1;
+        while(colors[0] == colors[i]) i--;
+
+        result = max(result, i);
+
+        return result;
+    }
+};
