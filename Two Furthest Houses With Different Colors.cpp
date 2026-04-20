ac3905247be8640ccbf2c0b8775b2aa1178f7232
ShubhamZoro <89676909+ShubhamZoro@users.noreply.github.com>
2026-04-20 07:56:41 +0530

    Optimize maxDistance function to O(n) 1 pass
    Refactor maxDistance function to use a single pass approach for improved efficiency.

--- Two Furthest Houses With Different Colors.cpp
+++ Two Furthest Houses With Different Colors.cpp
@@ -15,7 +15,7 @@ public:
     }
 };
 
-// O(n)
+// O(n) 2 pass
 
 class Solution {
 public:
@@ -35,5 +35,28 @@ public:
         result = max(result, i);
 
         return result;
+    }
+};
+
+// O(n) 1 pass
+
+class Solution {
+public:
+    int maxDistance(vector<int>& colors) {
+      int n = colors.size();
+
+        int result = 0;
+
+       for(int i = 0; i < n; i++) {
+            if(colors[i] != colors[0]) {
+                result = max(result, i);
+            }
+
+            if(colors[i] != colors[n-1]) {
+                result = max(result, abs(i - (n-1)));
+            }
+       }
+
+       return result;
     }
 };
